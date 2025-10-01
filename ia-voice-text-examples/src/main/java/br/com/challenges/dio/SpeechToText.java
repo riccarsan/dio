4c06d3e9f2f8cd2a9cf761726122c9df20eb0171
riccarsan <riccarsan@users.noreply.github.com>
2025-10-01 00:01:10 -0300

    Update SpeechToText.java

--- ia-voice-text-examples/src/main/java/br/com/challenges/dio/SpeechToText.java
+++ ia-voice-text-examples/src/main/java/br/com/challenges/dio/SpeechToText.java
@@ -7,30 +7,7 @@ import com.microsoft.cognitiveservices.speech.SpeechRecognizer;
 import com.microsoft.cognitiveservices.speech.audio.AudioConfig;
 
 public class SpeechToText {
-    public static void main(String[] args) {
-        try {
-            String speechKey = "";
-            String serviceRegion = "eastus";
-
-            SpeechConfig config = SpeechConfig.fromSubscription(speechKey, serviceRegion);
-            AudioConfig audioConfig = AudioConfig.fromDefaultMicrophoneInput();
-
-            SpeechRecognizer recognizer = new SpeechRecognizer(config, audioConfig);
-
-            System.out.println("Fale algo...");
-            SpeechRecognitionResult result = recognizer.recognizeOnceAsync().get();
-
-            if (result.getReason() == ResultReason.RecognizedSpeech) {
-                System.out.println("Você disse: " + result.getText());
-            } else {
-                System.out.println("Erro: " + result.toString());
-            }
-
-            result.close();
-            recognizer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+  
 }
+
 
