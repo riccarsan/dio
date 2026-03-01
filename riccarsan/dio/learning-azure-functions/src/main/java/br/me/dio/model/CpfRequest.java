d9b4b63290fa881d7bf4678cada61f98c1ef1408
riccarsan <riccarsan11@gmail.com>
2026-03-01 01:40:56 -0300

    criando um microservice servless

---
+++ riccarsan/dio/learning-azure-functions/src/main/java/br/me/dio/model/CpfRequest.java
@@ -0,0 +1,14 @@
+package br.me.dio.model;
+
+public class CpfRequest {
+
+    private String cpf;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
